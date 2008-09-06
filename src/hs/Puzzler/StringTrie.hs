92843726237a7e5b23c366664ce0f48fb1969a79
Denis Bueno <dbueno@gmail.com>
2008-09-06 16:38:30 -0600

    Delete the unused function `strings'; cleanup.

--- src/hs/Puzzler/StringTrie.hs
+++ src/hs/Puzzler/StringTrie.hs
@@ -9,11 +9,10 @@ module Puzzler.StringTrie
     , insert
     , insertWith
     , lookup
-    , strings
     , fromList )
     where
 
-import Data.Char( ord, chr )
+import Data.Char( ord )
 import Data.IntMap( IntMap )
 import Prelude hiding( lookup )
 import qualified Data.IntMap as Map
@@ -40,40 +39,14 @@ insert = insertWith (\new_value _old_value -> new_value)
 
 
 lookup :: String -> Trie a -> Maybe a
-lookup s (Trie m) = go m s ""
+lookup s t = go (unTrie t) s ""
     where
-      go m "" pfx = Nothing
-
+      go m [] pfx = Nothing
       go m (x:xs) pfx = {-# SCC "strings->go" #-} case Map.lookup (ord x) m of
         -- no such word with prefix:
         Nothing -> Nothing
         -- `pfx++[m]' is a word, with suffixes in `m':
         Just (Right ((Trie m),x)) -> if isLast then Just x else go m xs newpfx
-        -- `pfx++[m]' is not a word but may have suffixes which are:
-        Just (Left (Trie m)) -> go m xs newpfx
-
-        where newpfx = pfx++[x] :: String -- use diff list?
-              isLast = case xs of [] -> True ; _ -> False
-
-
--- | All the mappings starting with the given prefix.
-strings :: String -> Trie a -> [(String, a)]
-strings s (Trie m) = go m s ""
-    where
-      go m "" pfx =
-          {-# SCC "strings->go" #-}
-          -- recursively collect all the entries with "Right" as value
-          Map.foldWithKey (\k v ss -> case v of
-              Right ((Trie m),x) -> (pfx++[chr k],x) : (go m [] (pfx++[chr k]) ++ ss)
-              Left  (Trie m)     -> go m [] (pfx++[chr k]) ++ ss)
-            []
-            m
-
-      go m (x:xs) pfx = {-# SCC "strings->go" #-} case Map.lookup (ord x) m of
-        -- no such word with prefix:
-        Nothing -> []
-        -- `pfx++[m]' is a word, with suffixes in `m':
-        Just (Right ((Trie m),x)) -> (if isLast then ((newpfx,x) :) else id) $ go m xs newpfx
         -- `pfx++[m]' is not a word but may have suffixes which are:
         Just (Left (Trie m)) -> go m xs newpfx
 
@@ -87,9 +60,5 @@ fromList = foldr (uncurry insert) empty
 
 empty :: Trie a
 empty = Trie Map.empty
-
-
-trie = fromList [ ("an", ())
-                , ("a", ()) ]
 
 
