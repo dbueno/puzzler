d7124d7a6c2317c868679838a5def5ce535bb2d5
Denis Bueno <dbueno@gmail.com>
2008-09-06 13:47:28 -0600

    Add insertWith, implement lookup, generalise strings.
    (insertWith): Derived from insert (syntactically, not dynamically) and fixed a bug in it.
    (insert): Defined in terms of insertWith.
    (strings): Returns the mappings, not just the keys.

--- src/hs/Puzzler/StringTrie.hs
+++ src/hs/Puzzler/StringTrie.hs
@@ -6,6 +6,7 @@ module Puzzler.StringTrie
     , lookup
     , strings
     , insert
+    , insertWith
     , fromList
     , empty )
     where
@@ -17,18 +18,22 @@ import qualified Data.Map as Map
 newtype Trie a = Trie { unTrie :: Map Char (Either (Trie a) (Trie a, a)) }
     deriving (Eq, Ord, Show)
 
-lookup :: Trie a -> String -> Bool
-lookup = undefined
+lookup :: String -> Trie a -> Maybe a
+lookup s t = case strings s t of
+    []  -> Nothing
+    (result,x):_ | result == s -> Just x
+                 | otherwise   -> Nothing
 
 
--- | All the strings starting with the given prefix.
-strings :: String -> Trie a -> [String]
+-- | All the mappings starting with the given prefix, with the most matching one
+-- first (?).
+strings :: String -> Trie a -> [(String, a)]
 strings s (Trie m) = go m s ""
     where
       go m [] pfx =
           -- recursively collect all the entries with "Right" as value
           Map.foldWithKey (\k v ss -> case v of
-              Right ((Trie m),_) -> (pfx++[k]) : (go m [] (pfx++[k]) ++ ss)
+              Right ((Trie m),x) -> (pfx++[k],x) : (go m [] (pfx++[k]) ++ ss)
               Left  (Trie m) -> go m [] (pfx++[k]) ++ ss)
             []
             m
@@ -37,23 +42,29 @@ strings s (Trie m) = go m s ""
         -- no such word with prefix:
         Nothing -> []
         -- `pfx++[m]' is a word, with suffixes in `m':
-        Just (Right ((Trie m),_)) -> (if isLast then (newpfx :) else id) $ go m xs newpfx
+        Just (Right ((Trie m),x)) -> (if isLast then ((newpfx,x) :) else id) $ go m xs newpfx
         -- `pfx++[m]' is not a word but may have suffixes which are:
         Just (Left (Trie m)) -> go m xs newpfx
+
         where newpfx = pfx++[x] -- use diff list
               isLast = case xs of [] -> True ; _ -> False
 
 
-insert :: String -> a -> Trie a -> Trie a
-insert [] _ t = t
-insert (c:cs) x (Trie m) = Trie (Map.alter myAlter c m)
+insertWith :: (a -> a -> a) -> String -> a -> Trie a -> Trie a
+insertWith _ [] _ t = t
+insertWith f (c:cs) x (Trie m) = Trie (Map.alter myAlter c m)
     where
-      -- If `cs' is empty then we form a word.
+      -- If `cs' is empty then `c' is the last char of the word to insert; that
+      -- is, we're done.
       cons (t, x) = case cs of [] -> Right (t, x); _ -> Left t
 
-      myAlter Nothing              = Just $ cons (insert cs x empty, x)
-      myAlter (Just (Right (t,_))) = Just $ cons (insert cs x t, x)
-      myAlter (Just (Left  t))     = Just $ cons (insert cs x t, x)
+      myAlter Nothing               = Just $ cons (insertWith f cs x empty, x)
+      myAlter (Just (Right (t,x'))) = Just $ Right (insertWith f cs x t, f x x')
+      myAlter (Just (Left  t))      = Just $ cons (insertWith f cs x t, x)
+
+
+insert :: String -> a -> Trie a -> Trie a
+insert = insertWith (\new_value _old_value -> new_value)
 
 
 fromList :: [(String, a)] -> Trie a
@@ -64,6 +75,7 @@ empty :: Trie a
 empty = Trie Map.empty
 
 
-trie = fromList [("a", ()), ("an", ())]
+trie = fromList [ ("an", ())
+                , ("a", ()) ]
 
 
