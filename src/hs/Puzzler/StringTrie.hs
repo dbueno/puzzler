fac203dce42b94ee60886cf8085ae365c9828313
Denis Bueno <dbueno@gmail.com>
2008-09-06 16:33:34 -0600

    Implement with IntMaps instead of Maps; reimplement lookup on its own.
    The first, 'cause it was easy.
    
    The second resulted in a reasonable speedup and massively less memory cost.

--- src/hs/Puzzler/StringTrie.hs
+++ src/hs/Puzzler/StringTrie.hs
@@ -11,48 +11,18 @@ module Puzzler.StringTrie
     , fromList )
     where
 
-import Data.Map( Map )
+import Data.Char( ord, chr )
+import Data.IntMap( IntMap )
 import Prelude hiding( lookup )
-import qualified Data.Map as Map
+import qualified Data.IntMap as Map
 
-newtype Trie a = Trie { unTrie :: Map Char (Either (Trie a) (Trie a, a)) }
+newtype Trie a = Trie { unTrie :: IntMap (Either (Trie a) (Trie a, a)) }
     deriving (Eq, Ord, Show)
-
-lookup :: String -> Trie a -> Maybe a
-lookup s t = case strings s t of
-    []  -> Nothing
-    (result,x):_ | result == s -> Just x
-                 | otherwise   -> Nothing
-
-
--- | All the mappings starting with the given prefix, with the most matching one
--- first (?).
-strings :: String -> Trie a -> [(String, a)]
-strings s (Trie m) = go m s ""
-    where
-      go m [] pfx =
-          -- recursively collect all the entries with "Right" as value
-          Map.foldWithKey (\k v ss -> case v of
-              Right ((Trie m),x) -> (pfx++[k],x) : (go m [] (pfx++[k]) ++ ss)
-              Left  (Trie m) -> go m [] (pfx++[k]) ++ ss)
-            []
-            m
-
-      go m (x:xs) pfx = case Map.lookup x m of
-        -- no such word with prefix:
-        Nothing -> []
-        -- `pfx++[m]' is a word, with suffixes in `m':
-        Just (Right ((Trie m),x)) -> (if isLast then ((newpfx,x) :) else id) $ go m xs newpfx
-        -- `pfx++[m]' is not a word but may have suffixes which are:
-        Just (Left (Trie m)) -> go m xs newpfx
-
-        where newpfx = pfx++[x] -- use diff list?
-              isLast = case xs of [] -> True ; _ -> False
 
 
 insertWith :: (a -> a -> a) -> String -> a -> Trie a -> Trie a
 insertWith _ [] _ t = t
-insertWith f (c:cs) x (Trie m) = Trie (Map.alter myAlter c m)
+insertWith f (c:cs) x (Trie m) = Trie (Map.alter myAlter (ord c) m)
     where
       -- If `cs' is empty then `c' is the last char of the word to insert; that
       -- is, we're done.
@@ -65,6 +35,48 @@ insertWith f (c:cs) x (Trie m) = Trie (Map.alter myAlter c m)
 
 insert :: String -> a -> Trie a -> Trie a
 insert = insertWith (\new_value _old_value -> new_value)
+
+
+lookup :: String -> Trie a -> Maybe a
+lookup s (Trie m) = go m s ""
+    where
+      go m "" pfx = Nothing
+
+      go m (x:xs) pfx = {-# SCC "strings->go" #-} case Map.lookup (ord x) m of
+        -- no such word with prefix:
+        Nothing -> Nothing
+        -- `pfx++[m]' is a word, with suffixes in `m':
+        Just (Right ((Trie m),x)) -> if isLast then Just x else go m xs newpfx
+        -- `pfx++[m]' is not a word but may have suffixes which are:
+        Just (Left (Trie m)) -> go m xs newpfx
+
+        where newpfx = pfx++[x] :: String -- use diff list?
+              isLast = case xs of [] -> True ; _ -> False
+
+
+-- | All the mappings starting with the given prefix.
+strings :: String -> Trie a -> [(String, a)]
+strings s (Trie m) = go m s ""
+    where
+      go m "" pfx =
+          {-# SCC "strings->go" #-}
+          -- recursively collect all the entries with "Right" as value
+          Map.foldWithKey (\k v ss -> case v of
+              Right ((Trie m),x) -> (pfx++[chr k],x) : (go m [] (pfx++[chr k]) ++ ss)
+              Left  (Trie m)     -> go m [] (pfx++[chr k]) ++ ss)
+            []
+            m
+
+      go m (x:xs) pfx = {-# SCC "strings->go" #-} case Map.lookup (ord x) m of
+        -- no such word with prefix:
+        Nothing -> []
+        -- `pfx++[m]' is a word, with suffixes in `m':
+        Just (Right ((Trie m),x)) -> (if isLast then ((newpfx,x) :) else id) $ go m xs newpfx
+        -- `pfx++[m]' is not a word but may have suffixes which are:
+        Just (Left (Trie m)) -> go m xs newpfx
+
+        where newpfx = pfx++[x] :: String -- use diff list?
+              isLast = case xs of [] -> True ; _ -> False
 
 
 fromList :: [(String, a)] -> Trie a
