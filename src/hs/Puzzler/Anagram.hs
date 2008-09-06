f55f99f2aec3db2726558f1ffaf5dd658eef00ed
Denis Bueno <dbueno@gmail.com>
2008-09-06 09:40:57 -0600

    Rearrangement and cleanup; also, no more unsafePerformIO.

--- src/hs/Puzzler/Anagram.hs
+++ src/hs/Puzzler/Anagram.hs
@@ -1,38 +1,56 @@
 module Puzzler.Anagram
-    ( knuth
+    ( makeAnagramer
+    , knuth
     , anagrams )
     where
 
-import Control.Monad( filterM )
+import Control.Monad( filterM, liftM )
 import Data.List( find, sort )
 import Data.Array.IArray
-import System.IO.Unsafe( unsafePerformIO )
 
--- | The contents of @/usr/share/dict/words@, in an array.
---
--- This CAF uses `unsafePerformIO'.
-dictWords :: Array Int String
-dictWords =
-    let wds = words . unsafePerformIO $ readFile "/usr/share/dict/words"
-    in listArray (0, length wds - 1) wds
+type Dictionary = Array Int String
+data Anagramer = Anagramer
+    { dictWords :: Dictionary
+    , sortWords :: Dictionary }
 
--- | `dictWords', but with each string entry sorted.
-sortWords = amap sort dictWords
+makeAnagramer :: FilePath -> IO Anagramer
+makeAnagramer path = do
+    dw <- makeDictionary path
+    return $ Anagramer { dictWords = dw, sortWords = amap sort dw }
+
+-- | A dictionary manages a list of words.
+makeDictionary :: FilePath -> IO Dictionary
+makeDictionary path = do
+    dict <- words `liftM` readFile path
+    return $ listArray (0, length dict - 1) dict
+
+-- | An anagramer using the words from  @/usr/share/dict/words@.
+shareAna :: IO Anagramer
+shareAna = makeAnagramer "/usr/share/dict/words"
 
 -- | Returns a list of all the anagrams of the given string.
-knuth :: String -> [String]
-knuth s = 
+--
+-- The algorithm is attributed in various places on the 'net to Knuth in TAOCP
+-- Vol. III; hence the name.
+knuth :: Anagramer -> String -> [String]
+knuth a s = 
+    -- Find all indices which sort to (sort s).
     let s' = sort s
-        anagramIdxs = filter (\i -> sortWords!i == s') (range . bounds $ sortWords)
-    in map (dictWords!) anagramIdxs
+        anagramIdxs = filter (\i -> sw!i == s') (range . bounds $ sw)
+    in map (dw!) anagramIdxs
+  where 
+    sw = sortWords a ; dw = dictWords a
 
 -- | Returns anagrams of all substrings of given size.
-anagrams :: String -> Int -> [String]
-anagrams s size =
-    foldr (\ substring as -> knuth substring ++ as)
+anagrams :: Anagramer -> String -> Int -> [String]
+anagrams a s size =
+    foldr (\ substring as -> knuth a substring ++ as)
       []
       (filter (\s -> length s == size) $ powerset s)
 
+
+------------------------------------------------------------------------------
+-- Utilities
 
 powerset :: [a] -> [[a]]
 powerset = filterM (const [True, False])
