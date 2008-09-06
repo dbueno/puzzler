8e459a00fa9b354d70e6a3b1ab6cbca37cd074fb
Denis Bueno <dbueno@gmail.com>
2008-09-06 13:14:09 -0600

    Convert trie sets to maps.

--- src/hs/Puzzler/StringTrie.hs
+++ src/hs/Puzzler/StringTrie.hs
@@ -1,23 +1,34 @@
-module Puzzler.StringTrie where
+
+-- | Finite maps from strings to values, implemented with the traditional Trie
+-- structure.
+module Puzzler.StringTrie
+    ( Trie
+    , lookup
+    , strings
+    , insert
+    , fromList
+    , empty )
+    where
 
 import Data.Map( Map )
+import Prelude hiding( lookup )
 import qualified Data.Map as Map
 
-newtype Trie = Trie { unTrie :: Map Char (Either Trie Trie) }
+newtype Trie a = Trie { unTrie :: Map Char (Either (Trie a) (Trie a, a)) }
     deriving (Eq, Ord, Show)
 
-lookup :: Trie -> String -> Bool
+lookup :: Trie a -> String -> Bool
 lookup = undefined
 
 
 -- | All the strings starting with the given prefix.
-strings :: String -> Trie -> [String]
+strings :: String -> Trie a -> [String]
 strings s (Trie m) = go m s ""
     where
       go m [] pfx =
           -- recursively collect all the entries with "Right" as value
           Map.foldWithKey (\k v ss -> case v of
-              Right (Trie m) -> (pfx++[k]) : (go m [] (pfx++[k]) ++ ss)
+              Right ((Trie m),_) -> (pfx++[k]) : (go m [] (pfx++[k]) ++ ss)
               Left  (Trie m) -> go m [] (pfx++[k]) ++ ss)
             []
             m
@@ -26,33 +37,33 @@ strings s (Trie m) = go m s ""
         -- no such word with prefix:
         Nothing -> []
         -- `pfx++[m]' is a word, with suffixes in `m':
-        Just (Right (Trie m)) -> (if isLast then (newpfx :) else id) $ go m xs newpfx
+        Just (Right ((Trie m),_)) -> (if isLast then (newpfx :) else id) $ go m xs newpfx
         -- `pfx++[m]' is not a word but may have suffixes which are:
         Just (Left (Trie m)) -> go m xs newpfx
         where newpfx = pfx++[x] -- use diff list
               isLast = case xs of [] -> True ; _ -> False
 
 
-insert :: String -> Trie -> Trie
-insert [] t = t
-insert (c:cs) (Trie m) = Trie (Map.alter myAlter c m)
+insert :: String -> a -> Trie a -> Trie a
+insert [] _ t = t
+insert (c:cs) x (Trie m) = Trie (Map.alter myAlter c m)
     where
       -- If `cs' is empty then we form a word.
-      cons = case cs of [] -> Right ; _ -> Left
+      cons (t, x) = case cs of [] -> Right (t, x); _ -> Left t
 
-      myAlter Nothing          = Just $ cons (insert cs empty)
-      myAlter (Just (Right t)) = Just $ cons (insert cs t)
-      myAlter (Just (Left  t)) = Just $ cons (insert cs t)
-
-
-fromList :: [String] -> Trie
-fromList = foldr insert empty
+      myAlter Nothing              = Just $ cons (insert cs x empty, x)
+      myAlter (Just (Right (t,_))) = Just $ cons (insert cs x t, x)
+      myAlter (Just (Left  t))     = Just $ cons (insert cs x t, x)
 
 
-empty :: Trie
+fromList :: [(String, a)] -> Trie a
+fromList = foldr (uncurry insert) empty
+
+
+empty :: Trie a
 empty = Trie Map.empty
 
 
-trie = fromList ["a", "an"]
+trie = fromList [("a", ()), ("an", ())]
 
 
