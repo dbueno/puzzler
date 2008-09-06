45045604b4881a9adf34853e0f1da5bbc3297167
Denis Bueno <dbueno@gmail.com>
2008-09-05 22:31:43 -0600

    (anagrams): New function.

--- src/hs/Puzzler/Anagram.hs
+++ src/hs/Puzzler/Anagram.hs
@@ -1,10 +1,16 @@
-module Puzzler.Anagram where
+module Puzzler.Anagram
+    ( knuth
+    , anagrams )
+    where
 
+import Control.Monad( filterM )
 import Data.List( find, sort )
 import Data.Array.IArray
 import System.IO.Unsafe( unsafePerformIO )
 
 -- | The contents of @/usr/share/dict/words@, in an array.
+--
+-- This CAF uses `unsafePerformIO'.
 dictWords :: Array Int String
 dictWords =
     let wds = words . unsafePerformIO $ readFile "/usr/share/dict/words"
@@ -19,3 +25,14 @@ knuth s =
     let s' = sort s
         anagramIdxs = filter (\i -> sortWords!i == s') (range . bounds $ sortWords)
     in map (dictWords!) anagramIdxs
+
+-- | Returns anagrams of all substrings of given size.
+anagrams :: String -> Int -> [String]
+anagrams s size =
+    foldr (\ substring -> (knuth substring ++)) (filter (\s -> length s == size) $
+                                                 powerset s)
+      []
+
+
+powerset :: [a] -> [[a]]
+powerset = filterM (const [True, False])
