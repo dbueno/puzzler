2c5eac75e97027c0cf60ab7a4519f6d9435a52cc
Denis Bueno <dbueno@gmail.com>
2008-09-06 17:26:08 -0600

    Add a test using the local word  bank.

--- tests/TestDriver.hs
+++ tests/TestDriver.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Control.Exception( assert )
+import Control.Monad
 import Properties
 import Test.QuickCheck
 
@@ -7,4 +9,9 @@ config = defaultConfig{ configMaxTest = 1000 }
 qc = check config
 
 main = do
+    shareWords <- words `liftM` readFile "/usr/share/dict/words"
     qc prop_trie_lookup
+
+    putStr $ "prop_trie_lookup with /usr/share/dict/words ("
+             ++ show (length shareWords) ++ " words)..."
+    assert (prop_trie_lookup shareWords) $ putStrLn "done."
