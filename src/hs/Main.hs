eda4e03d4fab1831c0ef5ee4ae8b5d0fb391b34e
Denis Bueno <dbueno@gmail.com>
2008-09-13 13:59:44 -0600

    Allow file selection for dictionary -- done.

--- src/hs/Main.hs
+++ src/hs/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
 import Control.Monad( forM_ )
+import Data.IORef
 import Data.List( nub, intercalate, sort )
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Glade
+import System.Exit( ExitCode(ExitFailure), exitWith )
 import qualified Data.ByteString.Char8 as BS
 
-import Puzzler.Anagram( anagramsPat, createDictionary )
+import Puzzler.Anagram( Dictionary, anagramsPat, createDictionary, emptyDictionary )
 
 main :: IO ()
 main = do
@@ -21,31 +23,65 @@ main = do
     findAnagramsButton <- xmlGetWidget xml castToButton "findAnagramsButton"
     statusLabel <- xmlGetWidget xml castToLabel "statusLabel"
     let setStatus = makeSetStatus statusLabel
+    fileOpenMenuItem <- xmlGetWidget xml castToMenuItem "fileOpenMenuItem"
 
     -- Register events:
-    let dictFile = "data/mball.txt"
-    dict <- createDictionary "data/mball.txt"
-    anagramsPat dict "aoeu" "?" -- to load the dictionary
-      `seq` 
-      onClicked findAnagramsButton
-      (doFindAnagrams dict setStatus lettersEntry patternEntry anagramResultTextView)
-    setStatus $ "Loaded words from '" ++ dictFile ++ "'."
+    dictionaryRef <- newIORef emptyDictionary
+    doCreateDictionary dictionaryRef "data/mball.txt" setStatus
+--     readIORef dictionaryRef >>= \dict -> 
+--       anagramsPat dict "aoeu" "?" -- to load the dictionary
+--       `seq` 
+    onClicked findAnagramsButton
+      (doFindAnagrams setStatus dictionaryRef
+                      lettersEntry patternEntry anagramResultTextView)
+    
+    onActivateLeaf fileOpenMenuItem
+      (doOpenFileChooser dictionaryRef (Just window) setStatus)
 
     mainGUI -- always last
 
-doFindAnagrams dict setStatus lettersEntry patternEntry resultTextView =  do
+doFindAnagrams setStatus dictionaryRef lettersEntry patternEntry resultTextView =  do
     letters <- get lettersEntry entryText
     pattern <- get patternEntry entryText
     let findingText = "Finding anagrams for '" ++ letters
                       ++ "' of length " ++ show (length pattern) ++ "..."
     setStatus findingText
+    dict <- readIORef dictionaryRef
     let grams = nub . sort $ anagramsPat dict (BS.pack letters) (BS.pack pattern)
-    setStatus $ findingText ++ "done."
+    setStatus $ "Found " ++ show (length grams) ++ " '" ++ letters ++ "' anagrams."
     buffer <- get resultTextView textViewBuffer
     textBufferSetText buffer $ intercalate " " (map BS.unpack grams)
+
+doOpenFileChooser dictionaryRef maybeParent setStatus = do
+    dialog <- fileChooserDialogNew
+              (Just "Choose Dictionary")
+              maybeParent
+              FileChooserActionOpen
+              [("Select", ResponseOk)
+              ,("Cancel", ResponseCancel)]
+    response <- dialogRun dialog
+    case response of
+      ResponseOk -> do
+          maybePath <- fileChooserGetFilename dialog
+          case maybePath of
+            Just path -> doCreateDictionary dictionaryRef path setStatus
+            Nothing -> fatalExceptionDialog maybeParent 1
+                       $ "Could not get pathname from chooser dialog, exiting"
+      _ -> return ()
+    widgetDestroy dialog
+
+doCreateDictionary dictionaryRef path setStatus = do
+    setStatus $ "Using dictionary from '" ++ path ++ "'."
+    createDictionary path >>= writeIORef dictionaryRef
+    
+
+fatalExceptionDialog maybeParent code s = do
+    dialog <- messageDialogNew maybeParent [] MessageError ButtonsOk ("FATAL: " ++ s)
+    response <- dialogRun dialog
+    exitWith (ExitFailure code)
+    
 
 makeSetStatus statusLabel newText = do
     set statusLabel [ labelText := newText ]
     mainIteration -- TODO is this the right way to get the status to display
                   -- before finding the anagrams?
-
