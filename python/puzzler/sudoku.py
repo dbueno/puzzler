517da22abb6b5e8f05bf4dd5511007584d2ec217
Denis Bueno <dbueno@gmail.com>
2011-01-29 11:22:40 -0700

    bugfix: needed all variables for 3x3 constraints
    IF PYTHON CORRECTLY CREATED VARIABLE SCOPES THIS
    WOULD NEVER HAVE HAPPENED

--- python/puzzler/sudoku.py
+++ python/puzzler/sudoku.py
@@ -18,7 +18,7 @@ numUnique = 0
 N = 9
 # dictionary of [1-9][row][col]
 p = {}
-checkAssumps = []
+
 
 def showBoard(board):
   for row in range(0,N):
@@ -35,6 +35,9 @@ def showBoard(board):
   print ""
 
 
+# assumptions for solving. we use this so that uniqueness constraints from the
+# last board don't mess up solving the next board.
+checkAssumps = []
 def solveBoard(board):
   global numUnique, printResult, checkUnique, showUnique
   def assumeBoard(printBoard):
@@ -69,6 +72,7 @@ def solveBoard(board):
   if checkUnique:
     v = cnf.newVar('uniq')
     c = [-v]
+    c = []
     for j in range(0,N):
       for k in range(0,N):
         for i in range(0,N):
@@ -80,7 +84,7 @@ def solveBoard(board):
     checkAssumps.append(v)
     result = cnf.solve()
     if showUnique and result == cnf.RESULT_SAT:
-      print ""
+      print "non-unique board"
       showBoard(board)
     if result == cnf.RESULT_UNSAT:
       numUnique += 1
@@ -120,38 +124,47 @@ def setup():
           if i != l:
             cnf.addClause('<=1', [-p[i][j][k], -p[l][j][k]])
 
+  # columns have 1-9 at most once
   for i in range(0,N):
-    for j in range(0,N):
-      for k in range(0,N):
-        for l in range(0,N):
-          if l != j:
-            cnf.addClause('col_val', [-p[i][j][k], -p[i][l][k]])
+    for r in range(0,N):
+      for c in range(0,N):
+        for r2 in range(0,N):
+          if r2 != r:
+            cnf.addClause('col_val', [-p[i][r][c], -p[i][r2][c]])
 
+  # rows have 1-9 at most once
   for i in range(0,N):
-    for j in range(0,N):
-      for k in range(0,N):
-        for l in range(0,N):
-          if l != k:
-            cnf.addClause('row_val', [-p[i][j][k], -p[i][j][l]])
+    for r in range(0,N):
+      for c in range(0,N):
+        for c2 in range(0,N):
+          if c2 != c:
+            cnf.addClause('row_val', [-p[i][r][c], -p[i][r][c2]])
 
+  # 3x3 grids have 1-9 at most once
   for s in range(0,3):
     for t in range(0,3):
-      for j in range(s*3, (s+1)*3):
-        for k in range(t*3, (t+1)*3):
-          for l in range(s*3, (s+1)*3):
-            for m in range(t*3, (t+1)*3):
-              if j != l and k != m:
-                cnf.addClause('3x3', [-p[i][j][k], -p[i][l][m]])
+      for r in range(s*3, (s+1)*3):
+        for c in range(t*3, (t+1)*3):
+          for r2 in range(s*3, (s+1)*3):
+            for c2 in range(t*3, (t+1)*3):
+              # comment this out to see if your python doesn't suck.
+              # it doesn't error iff it sucks
+              for i in range(0,N):
+                if r != r2 and c != c2:
+                  # print "%d %d %d -- %d %d %d" % (i,r,c, i,r2,c2)
+                  cnf.addClause('3x3', [-p[i][r][c], -p[i][r2][c2]])
 
 if __name__ == "__main__":
   from optparse import OptionParser
   parser = OptionParser("usage: %prog [options] board-file")
-  parser.add_option("-b", action="store_false", dest="humanReadable",
+  parser.add_option("-b", action="store_false", dest="humanReadable", default=True,
                     help="print machine-readable board")
-  parser.add_option("-n", action="store_false", dest="printResult",
+  parser.add_option("-n", action="store_false", dest="printResult", default=True,
                     help="do not print board solutions")
-  parser.add_option("-s", action="store_true", dest="printStats",
+  parser.add_option("-s", action="store_true", dest="printStats", default=False,
                     help="print picosat stats at end")
+  parser.add_option("-u", action="store_true", dest="showUnique", default=False,
+                    help="show a second solution for non-unique board")
   # parser.add_option("-f", "--file", dest="filename",
   #                   help="write report to FILE", metavar="FILE")
   # parser.add_option("-q", "--quiet",
@@ -159,17 +172,21 @@ if __name__ == "__main__":
   #                   help="don't print status messages to stdout")
 
   (options, args) = parser.parse_args()
+  showUnique = options.showUnique
+  printResult = options.printResult
+  humanReadable = options.humanReadable
+  printStats = options.printStats
 
   if len(sys.argv) < 2:
     print "please supply a board file (one board per line) as first argument"
     parser.print_help()
     exit(1)
-  print "solving boards from '%s' ..." % sys.argv[1]
+  print "solving boards from '%s' ..." % args[0]
   if not printResult:
     print "set printResult in this script if you want the resulting boards"
   setup()
   c = 0
-  for line in file(sys.argv[1]):
+  for line in file(args[0]):
     if len(line) >= 9*9:
       # print "solving line %d '%s' ..." % (c, line)
       solveBoard(line)
